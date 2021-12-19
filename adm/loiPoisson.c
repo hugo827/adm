077b4aeda3f5a2c7c0fc40b0704aa1d88e5cc3bd
WangiWeberMDS <96393683+WangiWeberMDS@users.noreply.github.com>
2021-12-19 22:38:37 +0100

    Update loiPoisson.c
    correction si k==0 dans le calcul de la factoriel

--- adm/loiPoisson.c
+++ adm/loiPoisson.c
@@ -1,9 +1,11 @@
 #include "Header.h"
 
 double loiPoisson(double param, int k) {
-	int factorial = 1;
-	for (int i = 1; i <= k; i++) {
-		factorial *= i;
+	int factorial=1;
+	if(k!=0) {
+		for (int i = 1; i <= k; i++) {
+			factorial *= i;
+		}
 	}
 	return (exp(-param) * (pow(param, k))) / factorial;
 }
