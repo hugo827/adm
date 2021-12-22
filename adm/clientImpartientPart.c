1222c45e36e1eb3d48545a87b22fa349ba85f6d4
WangiWeberMDS <96393683+WangiWeberMDS@users.noreply.github.com>
2021-12-22 21:10:45 +0100

    message nbr de client impatient
    affiche un message avec le nombre de départ (meme si 0) , le message n'apparait qu'à partir de 10 minutes

--- adm/clientImpartientPart.c
+++ adm/clientImpartientPart.c
@@ -1,11 +1,12 @@
 #include "Header.h"
 
 
-void clientImpatientPart(Couts* couts, Client** pDebutFile) {
+void clientImpatientPart(Couts* couts, Client** pDebutFile, int afficherArriv) {
 
 	Client* pPrecClient = NULL;
 	Client* pFile = *pDebutFile;
 	int placeFile = 1;
+	int nbrClientPartis=0;
 
 	while (pFile != NULL) {
 		if (pFile->tempsPasseFile >= 10 && placeFile > 3) {
@@ -13,12 +14,16 @@ void clientImpatientPart(Couts* couts, Client** pDebutFile) {
 			pFile = pFile->pSuivClient;
 			coutDepartClientImpatient(pSauv, couts);
 			*pDebutFile = retirerClientFile(*pDebutFile, pSauv, &pPrecClient);
+			nbrClientPartis++;
 		}
 		else {
 			pPrecClient = pFile;
 			pFile = pFile->pSuivClient;
 		}
 		placeFile++;
+	}
+		if (afficherArriv) {
+		printf("Nombre de clients impatients partis: %d\n", nbrClientPartis);
 	}
 
 }
